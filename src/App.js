06c5d52a3c64ea616ab8274c201453c1296b59be
Your Name <you@example.com>
2023-04-18 15:10:17 -0400

    thirdcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './HomeScreen';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-           <HomeScreen/> 
+      <Router>
+        {/* switch */}
+        <Routes> 
+          <Route path="/" element={<HomeScreen />}>
+            {/* <HomeScreen /> */}
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
